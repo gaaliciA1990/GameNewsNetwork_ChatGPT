97e27d1ae4bf570e206c7d0a9d40f25b786d8351
Alicia <garcia.alicia1990@gmail.com>
2023-04-05 13:13:42 -0700

    Created model for new articles with new entry

--- src/main/kotlin/com/gamenews/models/Article.kt
+++ src/main/kotlin/com/gamenews/models/Article.kt
@@ -1,4 +1,29 @@
 package com.gamenews.models
 
-class Article {
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * Model class for the news articles
+ */
+class Article
+private constructor(
+    val id: Int,
+    var title: String,
+    var body: String
+) {
+    companion object {
+        // automatically generate unique ID
+        private val idCounter = AtomicInteger()
+
+        /**
+         * Create a new article with an id, title, and body
+         */
+        fun newEntry(title: String, body: String) = Article(idCounter.getAndIncrement(), title, body)
+    }
+
+    // mutable list of for storing articles
+    val articles = mutableListOf(Article.newEntry(
+        "Vanu has turned the tide",
+        "With the release of this app, Vanu grows stronger"
+    ))
 }
