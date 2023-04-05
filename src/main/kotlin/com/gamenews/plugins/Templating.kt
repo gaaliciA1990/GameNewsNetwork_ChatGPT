fc6cd4a53f4303a855056b3082447efbcfacc755
Alicia <garcia.alicia1990@gmail.com>
2023-04-05 13:04:24 -0700

    Added Template file and implemented templating code and output.

--- src/main/kotlin/com/gamenews/plugins/Templating.kt
+++ src/main/kotlin/com/gamenews/plugins/Templating.kt
@@ -1,4 +1,23 @@
 package com.gamenews.plugins
 
-class Templating {
+import io.ktor.server.freemarker.FreeMarker
+import freemarker.cache.CacheStorage
+import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
+import io.ktor.server.application.*
+
+/**
+ * Template settings to tell the app that FreeMarker templates are located in templates dir
+ *
+ * Output settings convert control chars provided by user to their corresponding HTML entities,
+ * which ensure strings are printed correctly with escaping and prevents XSS attacks
+ */
+fun Application.configureTemplating() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(
+            this::class.java.classLoader,
+            "templates"
+        )
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
 }
