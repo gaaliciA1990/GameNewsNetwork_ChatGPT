3dfe205935cc498e72c52b60bbe0f36ab71dbed1
Alicia <garcia.alicia1990@gmail.com>
2023-04-05 13:59:32 -0700

    Added get route for all articles. Added templates for header and displaying articles

--- src/main/kotlin/com/gamenews/plugins/Routing.kt
+++ src/main/kotlin/com/gamenews/plugins/Routing.kt
@@ -1,15 +1,46 @@
 package com.gamenews.plugins
 
+import com.gamenews.models.articles
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
+import io.ktor.server.freemarker.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondRedirect("articles")
+        }
+
+        route("articles") {
+            /**
+             * Get a list of articles.
+             */
+            get {
+                // Show a list of articles
+                call.respond(
+                    FreeMarkerContent(
+                        "index.ftl",
+                        mapOf("articles" to articles)
+                    )
+                )
+            }
+            get("new") {
+                // Show a page with fields for creating a new article
+            }
+            post {
+                // Save an article
+            }
+            get("{id}") {
+                // Show an article with a specific id
+            }
+            get("{id}/edit") {
+                // Show a page with fields for editing an article
+            }
+            post("{id}") {
+                // Update or delete an article
+            }
         }
     }
 }
---
+++ src/main/resources/templates/_layout.ftl
@@ -0,0 +1,16 @@
+<#macro header>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Game News Network</title>
+    </head>
+    <body style="text-align: center; font-family: sans-serif">
+    <img src="/static/ktor_logo.png">
+    <h1>PlanetSide2 News </h1>
+    <p><i>Powered by Ktor & Freemarker!</i></p>
+    <hr>
+    <#nested >
+    <a href="/">Back to main page</a>
+    </body>
+    </html>
+</#macro>
