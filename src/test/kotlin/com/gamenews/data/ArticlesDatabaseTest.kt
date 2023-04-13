fbba0dae6db945050bca9e7787c68014779828ba
Alicia <garcia.alicia1990@gmail.com>
2023-04-12 17:04:02 -0700

    Implemented tests for ArticlesDatabase class

--- src/test/kotlin/com/gamenews/data/ArticlesDatabaseTest.kt
+++ src/test/kotlin/com/gamenews/data/ArticlesDatabaseTest.kt
@@ -1,7 +1,9 @@
 package com.gamenews.data
 
 import com.gamenews.models.Article
+import com.mongodb.client.result.DeleteResult
 import com.mongodb.client.result.InsertOneResult
+import com.mongodb.client.result.UpdateResult
 import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -14,8 +16,12 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
+/**
+ * Unit tests for the ArticlesDB class
+ */
 class ArticlesDatabaseTest {
     lateinit var mockCollection: CoroutineCollection<Article>
     lateinit var articlesDB: ArticlesDatabase
@@ -69,16 +75,104 @@ class ArticlesDatabaseTest {
     fun `createArticle adds the new article to the db with the correct acknowledge`() = runTest {
         // SET UP
         val article: Article = Article.newEntry("New Article", "New test body")
-        val result = mockk<InsertOneResult>()
+        val ackResult = mockk<InsertOneResult>()
 
-        coEvery { mockCollection.insertOne(article) } returns result
-        coEvery { result.wasAcknowledged() } returns true
+        coEvery { mockCollection.insertOne(article, any()) } returns ackResult
+        coEvery { ackResult.wasAcknowledged() } returns true
 
         // DO
         val added = articlesDB.createArticle(article)
 
         // ASSERT
         assertTrue(added)
+    }
+
+    @Test
+    fun `getArticleById returns the article we query for`() = runTest {
+        // SET UP
+        val article1: Article = Article.newEntry("Test 1", "Test Body")
+
+        coEvery { mockCollection.findOneById(article1.id) } returns article1
+
+        // DO
+        val returnedArticle = articlesDB.getArticleById(article1.id)
+
+        // ASSERT
+        assertNotNull(returnedArticle)
+        assertEquals(article1, returnedArticle)
+        assertEquals(article1.title, returnedArticle.title)
+    }
+
+    @Test
+    fun `updateArticle changes the title of the article in the db`() = runTest {
+        // SET UP
+        val article1: Article = Article.newEntry("Test 1", "Test Body")
+        val updatedArticle = article1.copy(title = "New Test 1")
+        val ackResult = mockk<UpdateResult>()
+
+        // make sure our article is returned
+        coEvery { mockCollection.findOneById(article1.id) } returns article1
+
+        coEvery { mockCollection.updateOneById(article1.id, updatedArticle, any()) } returns ackResult
+        coEvery { ackResult.wasAcknowledged() } returns true
+
+        // DO
+        val updated = articlesDB.updateArticle(updatedArticle)
+
+        // ASSERT
+        assertTrue(updated)
+    }
+
+    @Test
+    fun `updateArticle returns false when the article is not in the db`() = runTest {
+        // SET UP
+        val article1: Article = Article.newEntry("Test 1", "Test Body")
+        val updatedArticle = article1.copy(title = "New Test 1")
+        val ackResult = mockk<UpdateResult>()
+
+        // make sure our article isn't found
+        coEvery { mockCollection.findOneById(article1.id) } returns null
+
+        // DO
+        val updated = articlesDB.updateArticle(updatedArticle)
+
+        // ASSERT
+        assertFalse(updated)
+    }
+
+    @Test
+    fun `deleteArticle deletes the article in the db by ID`() = runTest {
+        // SET UP
+        val article1: Article = Article.newEntry("Test 1", "Test Body")
+        val ackResult = mockk<DeleteResult>()
+
+        // make sure our article is returned
+        coEvery { mockCollection.findOneById(article1.id) } returns article1
+
+        coEvery { mockCollection.deleteOneById(article1.id) } returns ackResult
+        coEvery { ackResult.wasAcknowledged() } returns true
+
+        // DO
+        val deleted = articlesDB.deleteArticle(article1.id)
+
+        // ASSERT
+        assertTrue(deleted)
+    }
+
+    @Test
+    fun `deleteArticle returns false when the article is not in the db`() = runTest {
+        // SET UP
+        val article1: Article = Article.newEntry("Test 1", "Test Body")
+        val ackResult = mockk<DeleteResult>()
+
+        // make sure our article isn't found
+        coEvery { mockCollection.findOneById(article1.id) } returns null
+
+        // DO
+        val updated = articlesDB.deleteArticle(article1.id)
+
+        // ASSERT
+        assertFalse(updated)
     }
 }
 // SET UP
